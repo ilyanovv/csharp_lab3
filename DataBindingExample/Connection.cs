0bc5b36c206f6bae32876670badd2f424a917b92
ilyanovv <ilyanovv@gmail.com>
2017-12-28 22:31:12 +0300

    SQLite data storage

---
+++ DataBindingExample/Connection.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLitePCL;
+
+namespace DataBindingExample
+{
+    public class Connection
+    {
+        public SQLiteConnection connection { get; }
+        protected Connection() {
+            connection = new SQLiteConnection("Storage.db");
+            string sql = @"CREATE TABLE IF NOT EXISTS People (
+                                                Id INTEGER NOT NULL PRIMARY KEY,
+                                                Name TEXT,
+                                                Birthday TEXT,
+                                                Male INTEGER CHECK (Male IN (0, 1)) DEFAULT 1);";
+            using (var statement = connection.Prepare(sql))
+            {
+                statement.Step();
+            }
+            System.Diagnostics.Debug.WriteLine("connection = " + connection.ToString());
+        }
+
+        private sealed class SingletonCreator
+        {
+            private static readonly Connection instance = new Connection();
+            public static Connection Instance { get { return instance; } }
+        }
+
+        public static Connection Instance
+        {
+            get { return SingletonCreator.Instance; }
+        }
+
+    }
+}
---
+++ DataBindingExample/DBUtils.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SQLitePCL;
+
+
+namespace DataBindingExample
+{
+    public class DBUtils
+    {
+        public static void Add(Person person)
+        {
+            SQLiteConnection connection = Connection.Instance.connection;
+            using (var statement = connection.Prepare("INSERT INTO People (Name, Birthday, Male) VALUES (?, ?, ?)"))
+            {
+                statement.Bind(1, person.Name == null ? " " : person.Name);
+                statement.Bind(2, person.Birthday.ToString());
+                statement.Bind(3, person.Male ? 1 : 0);
+                statement.Step();
+            }
+        }
+
+        public static void Delete(Person person)
+        {
+            SQLiteConnection connection = Connection.Instance.connection;
+            using (var statement = connection.Prepare("DELETE FROM People WHERE Id=?"))
+            {
+                statement.Bind(1, person.Id);
+                statement.Step();
+            }
+        }
+
+        public static void Update(Person person, String FieldToUpdate)
+        {
+            SQLiteConnection connection = Connection.Instance.connection;
+            using (var statement = connection.Prepare("UPDATE People SET " + FieldToUpdate + " = ? WHERE Id=?"))
+            {
+                if (person.Id == null)
+                    return;
+                object newValue = person.GetType().GetProperty(FieldToUpdate).GetValue(person);
+                if (FieldToUpdate.Equals("Name") && newValue == null)
+                {
+                    newValue = " ";
+                }
+                statement.Bind(1, newValue.ToString());
+                statement.Bind(2, person.Id);
+                statement.Step();
+            }
+        }
+
+        public static void LoadAllPersons(Persons persons)
+        {
+            SQLiteConnection connection = Connection.Instance.connection;
+            using (var statement = connection.Prepare("SELECT Id, Name, Birthday, Male FROM People"))
+            {
+                while (statement.Step() == SQLiteResult.ROW)
+                {
+                    System.Diagnostics.Debug.WriteLine(
+                        "id= " + ((long)statement[0]).ToString() + " " +
+                        (string)statement[1] + " "
+                        + (string)statement[2] + " " +
+                        ((long)statement[3]).ToString()
+                        + Environment.NewLine);
+
+                    persons.Add(new Person()
+                    {
+                        Id = (int)(long)statement[0],
+                        Avatar = null,
+                        Name = (string)statement[1],
+                        Male = (long)statement[3] == 1,
+                        Birthday = !((string)statement[2]).Equals("") ? (DateTime?)DateTime.Parse((string)statement[2]) : null
+                    });
+
+                }
+            }
+        }
+    }
+}
